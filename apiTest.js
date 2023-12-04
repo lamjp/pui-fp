7c3aa0ae7fdb582573c4772beee4fff594c1c09b
jayplam <jplam@andrew.cmu.edu>
2023-12-04 09:34:17 -0500

    Commit 1

---
+++ apiTest.js
@@ -0,0 +1,29 @@
+const token = '6302e0c48f064125a6c0aebd2d33ca64';
+
+const clientId = token; // Replace with your client ID
+const code = undefined;
+
+if (!code) {
+    redirectToAuthCodeFlow(clientId);
+} else {
+    const accessToken = await getAccessToken(clientId, code);
+    const profile = await fetchProfile(accessToken);
+    populateUI(profile);
+}
+
+async function redirectToAuthCodeFlow(clientId) {
+    // TODO: Redirect to Spotify authorization page
+}
+
+async function getAccessToken(clientId, code) {
+  // TODO: Get access token for code
+}
+
+async function fetchProfile(token) {
+    // TODO: Call Web API
+}
+
+function populateUI(profile) {
+    // TODO: Update UI with profile data
+}
+
